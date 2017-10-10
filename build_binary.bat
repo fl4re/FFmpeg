0f071e40c311d1d654c144abbf08f44d5e21fba5
Mark Barnes <mark.barnes@starbreeze.com>
2017-10-09 18:21:22 -0700

    npm package of 3.3.4

---
+++ build_binary.bat
@@ -0,0 +1,26 @@
+setlocal EnableDelayedExpansion
+
+@echo off
+
+
+set TOPLEVEL=%~dp0
+set OLDPATH=%path%
+set MSYS2_PATH_TYPE=inherit
+
+call "%VS140COMNTOOLS%../../VC/bin/amd64/vcvars64.bat"
+if errorlevel 1 exit /b %errorlevel%
+
+
+start "minttyffmpeg" /B /WAIT %MSYS64%\usr\bin\mintty.exe -o daemonize=false -i /msys2.ico /usr/bin/bash --login -c "'%TOPLEVEL%'"/setup_ffmpeg_msvc.sh
+if errorlevel 1 exit /b %errorlevel%
+
+set PATH=%OLDPATH%
+
+
+if exist "%TOPLEVEL%"setup_ffmpeg_msvc.log (
+type "%TOPLEVEL%"setup_ffmpeg_msvc.log
+del /F /Q "%TOPLEVEL%"setup_ffmpeg_msvc.log
+exit /b 1
+)
+
+exit /b 0
---
+++ setup_ffmpeg_msvc.sh
@@ -0,0 +1,45 @@
+HomeDir="$(dirname $(readlink -f $0))"
+ItIsATrap() {
+	errcode=$?
+    echo "FFmpeg build error! Exit code: $errcode " > $HomeDir/setup_ffmpeg_msvc.log
+    echo ", the command executing at the time of the error was " >> $HomeDir/setup_ffmpeg_msvc.log
+    echo "$BASH_COMMAND" >> $HomeDir/setup_ffmpeg_msvc.log
+    exit $errcode
+}
+rm -rf $HomeDir/setup_ffmpeg_msvc.log
+
+cd "$VCINSTALLDIR"/BIN/amd64/
+ToolChainDir="$(pwd)"
+cd $HomeDir
+
+trap ItIsATrap ERR TERM INT
+
+# There is no need to clean before build
+# Jenkins always delete everything before build
+# Local build should not have to delete for efficiency
+
+
+#echo -n "Cleaning FFmpeg binaries..."
+#rm -rf ./win64
+#mkdir ./win64
+#echo "Done!"
+#echo -n "Cleaning FFmpeg..."
+
+#trap - ERR TERM INT
+#make -i clean > /dev/null
+#trap ItIsATrap ERR TERM INT
+#echo "Done!"
+
+echo -n "Configuring FFmpeg..."
+PATH=$ToolChainDir:$PATH ./configure --cpu=i686 --toolchain=msvc --enable-shared --disable-gpl --disable-programs --disable-doc --enable-avresample --prefix=./win64 $FL4RE_FFMPEG_CONFIGURE_FLAGS
+echo "Done!"
+echo -n "Making FFmpeg..."
+PATH=$ToolChainDir:$PATH make -j8 $FL4RE_FFMPEG_MAKE_FLAGS
+echo "Done!"
+echo -n "Installing FFmpeg..."
+PATH=$ToolChainDir:$PATH make install $FL4RE_FFMPEG_MAKE_INSTALL_FLAGS
+echo "Done!"
+echo -n "Patching FFmpeg libs..."
+cd ./win64
+for fname in lib/*.def; do FNAME_NP=${fname#*/}; FNAME_NN=${FNAME_NP%%-*}; PATH=$ToolChainDir:$PATH ${FL4RE_FFMPEG_WIN_LIB_TOOL:=lib} /MACHINE:x64 /DEF:$fname /OUT:bin/$FNAME_NN.lib; done
+echo "Done!"
