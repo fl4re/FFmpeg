4b2c855fc64a5de3a6d7704650aa6c5146d5853a
Mark Barnes <mark.barnes@starbreeze.com>
2017-10-12 19:56:41 -0700

    Fix publishing scripts

--- publish_binary_on_mac.sh
+++ publish_binary_on_mac.sh
@@ -3,13 +3,7 @@
 # make sure we are inside a git checkout
 git rev-parse --is-inside-work-tree
 
-# install deps
-npm install
-
-# npm version patch w/o git commit & git tag
-npm --no-git-tag-version version patch
-
 # finally prades publish
-node_modules/.bin/prades publish -v
+prades publish -v
 
 exit 0
